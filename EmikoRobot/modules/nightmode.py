27204740b8cac706f851a045a319ab8c6880ab8c
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-01-21 18:27:33 +0530

    Update nightmode.py

--- EmikoRobot/modules/nightmode.py
+++ EmikoRobot/modules/nightmode.py
@@ -129,7 +129,7 @@ async def job_close():
         try:
             await tbot.send_message(
                 int(pro.chat_id),
-                "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By @emiexrobot**",
+                "12:00 Am, Group Is Closing Till 6 Am. Night Mode Started ! \n**Powered By @Missharelyluna_bot**",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
@@ -154,7 +154,7 @@ async def job_open():
         try:
             await tbot.send_message(
                 int(pro.chat_id),
-                "06:00 Am, Group Is Opening.\n**Powered By @emiexrobot**",
+                "06:00 Am, Group Is Opening.\n**Powered By @Missharelyluna_bot**",
             )
             await tbot(
                 functions.messages.EditChatDefaultBannedRightsRequest(
