13b1a5a8f2c31050961a1ca7e0a0ca2d5229ef64
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-22 11:34:46 +0530

    Update forcesubs.py

--- EmikoRobot/modules/forcesubs.py
+++ EmikoRobot/modules/forcesubs.py
@@ -179,20 +179,20 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❂ Emiko can mute members who are not subscribed your channel until they subscribe
-❂ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
-❂*Setup*
+☼︎ Emiko can mute members who are not subscribed your channel until they subscribe
+☼︎ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
+☼︎*Setup*
 *Only creator*
-❂ Add me in your group as admin
-❂ Add me in your channel as admin 
+☼︎ Add me in your group as admin
+☼︎ Add me in your channel as admin 
  
 *Commmands*
-❂ /fsub {channel username} - To turn on and setup the channel.
+☼︎ /fsub {channel username} - To turn on and setup the channel.
   💡Do this first...
-❂ /fsub - To get the current settings.
-❂ /fsub disable - To turn of ForceSubscribe..
+☼︎ /fsub - To get the current settings.
+☼︎ /fsub disable - To turn of ForceSubscribe..
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
-❂ /fsub clear - To unmute all members who muted by me.
+☼︎ /fsub clear - To unmute all members who muted by me.
 *Federation*
 Everything is fun, until a spammer starts entering your group, and you have to block it. Then you need to start banning more, and more, and it hurts.
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?\n
