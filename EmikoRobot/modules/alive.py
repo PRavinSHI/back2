7f509f3a13d516757abcdbb52ae47ff49c08c37a
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-22 10:47:33 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,22 +9,22 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/7e4eade9cce789f23cbcd.jpg"
+PHOTO = "https://telegra.ph/file/ba52aa96c15a1f3c11e38.jpg"
 
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm LUNA .** \n\n"
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm ROSI .** \n\n"
     TEXT += "⚪ **I'm Working Properly** \n\n"
-    TEXT += f"⚪ **My Master : [ALAN WALKER](https://t.me/alpha_romeo_06)** \n\n"
+    TEXT += f"⚪ **My Master : [𝐏𝐑𝐀𝐕𝐈𝐍𝐂𝐘](https://t.me/Gplove_Rp)** \n\n"
     TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
     TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
     TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-    TEXT += "**Thanks For Adding Me Here ❤️**"
+    TEXT += "**Thanks For Adding Me Here 👑**"
     BUTTON = [
         [
-            Button.url("Help", "https://t.me/Missharelyluna_bot?start=help"),
-            Button.url("Support", "https://t.me/lunasupportz"),
+            Button.url("Help🕴️", "https://t.me/Rosiultra_bot?start=help"),
+            Button.url("Support🤝", "https://t.me/Rosisupport"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
