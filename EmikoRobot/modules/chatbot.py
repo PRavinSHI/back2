641b3afa65c63a22d995fef9433b2530c7d9cb7d
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-23 10:50:54 +0530

    Update chatbot.py

--- EmikoRobot/modules/chatbot.py
+++ EmikoRobot/modules/chatbot.py
@@ -499,10 +499,10 @@ async def inuka(client, message):
 
 
 __help__ = """
-❂ Emiko AI is the only ai system which can detect & reply upto 200 language's
+☼︎ Emiko AI is the only ai system which can detect & reply upto 200 language's
 
-❂ /chatbot [ON/OFF]: Enables and disables AI Chat mode.
-❂ /chatbot EN : Enables English only chatbot.
+☼︎ /chatbot [ON/OFF]: Enables and disables AI Chat mode.
+☼︎ /chatbot EN : Enables English only chatbot.
 """
 
 __mod_name__ = "Chatbot"
