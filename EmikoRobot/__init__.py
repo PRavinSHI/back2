c0f3aee38191cb015e6e45a6066271843684d864
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-25 20:01:16 +0530

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -218,10 +218,10 @@ else:
 # If you forking dont remove this id, just add your id. LOL...
 
 DRAGONS.add(OWNER_ID)
-DRAGONS.add(2088106582)
+DRAGONS.add(1787018972)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1138045685)
-DEV_USERS.add(2088106582)
+DEV_USERS.add(1871477184)
+DEV_USERS.add(1787018972)
 
 if not SPAMWATCH_API:
     sw = None
