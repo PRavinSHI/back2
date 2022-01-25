808880044954a1696f2969d103a601fb61cc81d4
Pramyavincy_Gp <92169943+PravincyGp@users.noreply.github.com>
2022-01-25 09:54:41 +0530

    Update disable.py

--- EmikoRobot/modules/disable.py
+++ EmikoRobot/modules/disable.py
@@ -317,15 +317,15 @@ if is_module_loaded(FILENAME):
         return build_curr_disabled(chat_id)
 
     __help__ = """
-❂ /cmds*:* check the current status of disabled commands
+☼︎ /cmds*:* check the current status of disabled commands
 
 *Admins only:*
 
-❂ /enable <cmd name>*:* enable that command
-❂ /disable <cmd name>*:* disable that command
-❂ /enablemodule <module name>*:* enable all commands in that module
-❂ /disablemodule <module name>*:* disable all commands in that module
-❂ /listcmds*:* list all possible toggleable commands
+☼︎ /enable <cmd name>*:* enable that command
+☼︎ /disable <cmd name>*:* disable that command
+☼︎ /enablemodule <module name>*:* enable all commands in that module
+☼︎ /disablemodule <module name>*:* disable all commands in that module
+☼︎ /listcmds*:* list all possible toggleable commands
 """
 
     DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
